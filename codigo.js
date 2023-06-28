eb3048878c7c8f399bb509ff6e5a8945070978d9
SebastianSR <102806578+Alucinado@users.noreply.github.com>
2023-06-28 16:06:55 -0500

    Initial commit

---
+++ codigo.js
@@ -0,0 +1,8 @@
+// Obtener el elemento HTML por su ID
+// Obtener el elemento HTML por su ID
+var boton = document.getElementById("miBoton");
+
+// Agregar un evento de clic al botón
+boton.addEventListener("click", function () {
+    alert("¡Hola! Has hecho clic en el botón.");
+});
